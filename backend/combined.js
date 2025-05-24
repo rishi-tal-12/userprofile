df7bca5608ba6566912de178e35dd37a7f34bee7
rishi-tal-12 <rishi_t@me.iitr.ac.in>
2025-05-24 22:33:31 +0530

    all combined into one

---
+++ backend/combined.js
@@ -0,0 +1,143 @@
+import Moralis from 'moralis';
+
+ async function getuserdetails() {
+
+let requireddefi=[];
+
+  await Moralis.start({
+    apiKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjJjYWY5NGVhLTEzYWEtNDkyNy05YjI2LTQ0NzE0MTgyODdlNSIsIm9yZ0lkIjoiNDQ4NTU4IiwidXNlcklkIjoiNDYxNTEwIiwidHlwZUlkIjoiYmE2NDFjNTctZmNmOS00ZmI1LTg1YWYtZTg2OGUzZDkwOThhIiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3NDc5MTk0MTksImV4cCI6NDkwMzY3OTQxOX0.kxF6mXIRPOWXQ1H-sPtDJRqlsh5G_EwPVSpHl4bK42g"
+  });
+
+try {
+
+  const responsedefi = await Moralis.EvmApi.wallets.getDefiPositionsSummary({
+    "chain": "0x1",
+    "address": "0xd100d8b69c5ae23d6aa30c6c3874bf47539b95fd"
+  });
+
+  requireddefi= responsedefi.raw.map((item)=>{
+    return{
+      protocol_name:item.protocol_name,
+      position:item.position.label,
+      position_balance_usd:item.position.balance_usd
+    }
+  })
+
+  console.log(responsedefi.raw);
+} catch (e) {
+  console.error(e);
+}
+
+
+let requirednetworth_usd;
+
+try {
+
+  const responsenetworth = await Moralis.EvmApi.wallets.getWalletNetWorth({
+    "excludeSpam": true,
+    "excludeUnverifiedContracts": true,
+    "maxTokenInactivity": 1,
+    "minPairSideLiquidityUsd": 1000,
+    "address": "0xd100d8b69c5ae23d6aa30c6c3874bf47539b95fd"
+  });
+
+ requirednetworth_usd=responsenetworth.raw.total_networth_usd;
+
+  console.log(responsenetworth.raw);
+} catch (e) {
+  console.error(e);
+}
+
+
+let totalnfts;
+let requirednft=[];
+try {
+
+  const responsenft = await Moralis.EvmApi.nft.getWalletNFTs({
+    "chain": "0x1",
+    "format": "decimal",
+    "normalizeMetadata": true,
+    "mediaItems": false,
+    "address": "0xd100d8b69c5ae23d6aa30c6c3874bf47539b95fd"
+  });
+  
+  totalnfts=responsenft.raw.result.length;
+ requirednft=responsenft.raw.result.map((item)=>{
+  return{
+    name:item.name,
+    verified:item.verified_collection,
+    rarity:item.rarity_percentage,
+    floor_price:item.floor_price_usd
+  }
+ });
+  
+  console.log(responsenft.raw);
+} catch (e) {
+  console.error(e);
+}
+
+
+let requiredpnl;
+
+try {
+
+  const responsepnl = await Moralis.EvmApi.wallets.getWalletProfitabilitySummary({
+    "chain": "0x1",
+    "address": "0xd100d8b69c5ae23d6aa30c6c3874bf47539b95fd"
+  });
+
+   requiredpnl={ 
+      total_profit_usd: responsepnl.raw.total_realized_profit_usd,
+      total_profit_perecntage: responsepnl.raw.total_realized_profit_percentage
+};
+
+  console.log(responsepnl.raw);
+} catch (e) {
+  console.error(e);
+}
+
+let requiredtoken =[];
+let totaltokens;
+
+try {
+
+  const responsetoken = await Moralis.EvmApi.token.getWalletTokenBalances({
+    "chain": "0x1",
+    "address": "0xd100d8b69c5ae23d6aa30c6c3874bf47539b95fd"
+  });
+  totaltokens=responsetoken.raw.length;
+  requiredtoken= responsetoken.raw.map((item)=>{
+    return{
+      token_name:item.name,
+      token_balance:item.balance/10**(item.decimals)
+    }
+  });
+
+  console.log(responsetoken.raw);
+} catch (e) {
+  console.error(e);
+}
+
+console.log(requireddefi);
+console.log(totalnfts);
+console.log(requirednft);
+console.log(requiredpnl);
+console.log(totaltokens);
+console.log(requiredtoken);
+console.log(requirednetworth_usd);
+
+let address="0xd100d8b69c5ae23d6aa30c6c3874bf47539b95fd";
+
+return{
+    useraddress:address,
+    userdefi:requireddefi,
+    totalusernft:totalnfts,
+    totalusernftinfo:requirednft,
+    totalpnl:requiredpnl,
+    totalusertokens:totaltokens,
+    totalusertokeninfo:requiredtoken,
+    totalusernetworth:requirednetworth_usd
+};
+ }
+ export{getuserdetails};
+
---
+++ backend/test.js
@@ -0,0 +1,4 @@
+import { getuserdetails } from './combined.js';
+
+const userdetails1 = await getuserdetails();
+console.log(userdetails1);
